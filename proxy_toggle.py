dacd5d1b3a5272a897979b46e90b88df17070e5b
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-11-30 18:38:26 +0530

    feat: Enhance set_proxy function to configure Git and NPM with proxy settings

--- proxy_toggle.py
+++ proxy_toggle.py
@@ -9,3 +9,19 @@ def run_command(cmd):
     if result.returncode != 0:
         print("⚠️  Error running command.")
     return result.returncode
+
+def set_proxy():
+    print("\n=== Setting Proxy ===")
+
+    # Git
+    run_command(f'git config --global http.proxy {PROXY}')
+    run_command(f'git config --global https.proxy {PROXY}')
+
+    # NPM
+    run_command(f'npm config set proxy {PROXY}')
+    run_command(f'npm config set https-proxy {PROXY}')
+    run_command('npm config set strict-ssl false')
+
+    print("\n✅ Proxy has been SET successfully.")
+
+
